8fc2b15ddf2d8e20fb533a353f3dd550f441a4aa
rmahajan466 <rmahajan466@gmail.com>
2025-03-25 01:55:21 +0530

    Started Developing Vedant Portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Architect", "Graphic Designer", "Interior Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Vedant`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Architect", "Graphic Designer", "Interior Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>
+                    I'm a visionary architect with a passion for designing spaces that seamlessly blend functionality, aesthetics, and sustainability. With years of experience in the field, he specializes in creating innovative architectural solutions that reflect both modern trends and timeless elegance.
+                    <br />
+                    From residential homes to commercial structures, Vedant approaches each project with a keen eye for detail and a deep understanding of client needs. His expertise in architectural design, urban planning, and interior spaces ensures that every structure he designs is not just visually stunning but also practical and sustainable.
+                    <br />
+                    Driven by creativity and precision, Vedant Hire transforms ideas into reality, crafting spaces that inspire and elevate the way people live and work.
+                  </p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
