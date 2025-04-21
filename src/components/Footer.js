550846c5647801fcf93c75cd4ec6db9b00e9a1da
rmahajan466 <rmahajan466@gmail.com>
2025-04-21 17:17:35 +0530

    Added: Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon3.svg";
 import { INSTA_URL } from "../utils.js";
